7ccaa3c43f2be9273d070381207f9fa7a0d78220
MikkoTur <mikko.turppo@hotmail.com>
2018-05-16 13:11:44 +0300

    Added simple REST API interface , getting contacts from a server is supported

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Contact} from '../contact';
-import {ContactService} from '../contact.service';
+import {ContactService} from '../services/contact.service';
 
 @Component({
   selector: 'cw-contact-list',
@@ -16,7 +16,10 @@ export class ContactListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.contacts = this.contactService.getContacts();
+    this.contactService.getContacts().subscribe(response => {
+      this.contacts = response;
+      console.log(this.contacts);
+    });
   }
 
 }
