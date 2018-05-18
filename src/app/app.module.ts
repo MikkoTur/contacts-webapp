7ac1c99920a5b496dab91e49227640d2473b025e
MikkoTur <mikko.turppo@hotmail.com>
2018-05-18 10:40:29 +0300

    added visual changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import {ContactHttpService} from './contact/services/contact-http.service';
 import {HttpClientModule} from '@angular/common/http';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule} from '@angular/material';
+import {MaterialComponentsModule} from './ui/material-components/material-components.module';
 
 const appRoutes: Routes = [
   { path: 'contacts', component: ContactListComponent},
@@ -27,7 +29,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MaterialComponentsModule
   ],
   providers: [
     ContactService,
